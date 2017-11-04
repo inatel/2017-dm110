3784fccb5be740003e7c8a6aa211011be480f391
Marcio Azevedo <mecva.mecva@gmail.com>
2017-11-04 14:56:19 -0200

    Adding a POST service method creating a JSON.

--- dm110-impl/src/main/java/br/inatel/dm110/hello/impl/HelloServiceImpl.java
+++ dm110-impl/src/main/java/br/inatel/dm110/hello/impl/HelloServiceImpl.java
@@ -1,12 +1,23 @@
 package br.inatel.dm110.hello.impl;
 
 import br.inatel.dm110.hello.api.HelloService;
+import br.inatel.dm110.hello.api.Result;
 
 public class HelloServiceImpl implements HelloService {
 
 	@Override
 	public String sayHello(String name) {
 		return "<h1>Hello " + name + "!!!</h1>";
+	}
+
+	@Override
+	public Result message(String first, String last) {
+		Result result = new Result();
+		result.setFirstName(first);
+		result.setLastName(last);
+		String message = String.format("Hello %s %s!!!", first, last);
+		result.setMessage(message);
+		return result;
 	}
 
 }
