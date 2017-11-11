500d5cc31729a2bba5e155071421ffa5a872ec72
Marcio Azevedo <mecva.mecva@gmail.com>
2017-11-11 16:38:31 -0200

    Fixing typo on servlet class name.

--- dm110-war/src/main/java/br/inatel/dm110/hello/servlet/HelloServlet.java
+++ dm110-war/src/main/java/br/inatel/dm110/hello/servlet/HelloServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/hello")
-public class HeloServlet extends HttpServlet {
+public class HelloServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -24118939727042992L;
 
