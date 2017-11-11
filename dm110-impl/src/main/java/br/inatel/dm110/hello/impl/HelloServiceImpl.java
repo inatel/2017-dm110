0e9cfbe844c5131320bf11540000eff6b6d62ceb
Marcio Azevedo <mecva.mecva@gmail.com>
2017-11-11 11:29:38 -0200

    Creating a simple EJB.

--- dm110-impl/src/main/java/br/inatel/dm110/hello/impl/HelloServiceImpl.java
+++ dm110-impl/src/main/java/br/inatel/dm110/hello/impl/HelloServiceImpl.java
@@ -1,13 +1,22 @@
 package br.inatel.dm110.hello.impl;
 
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+
 import br.inatel.dm110.hello.api.HelloService;
 import br.inatel.dm110.hello.api.Result;
+import br.inatel.dm110.hello.interfaces.HelloRemote;
 
+@RequestScoped
 public class HelloServiceImpl implements HelloService {
+
+	@EJB(lookup = "java:app/dm110-ejb-0.1-SNAPSHOT/HelloBean!br.inatel.dm110.hello.interfaces.HelloRemote")
+	private HelloRemote hello;
 
 	@Override
 	public String sayHello(String name) {
-		return "<h1>Hello " + name + "!!!</h1>";
+		String message = hello.sayHello(name);
+		return "<h1>" + message + "</h1>";
 	}
 
 	@Override
